7bf6ac95d76a5b90d67be12f21a330b52e857f9d
cc <sadako114ever@gmail.com>
2022-04-26 18:51:58 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# duke_W2_OS
+Create a Reporting Script for File Sizes
+
+Specific instructions for the project
+In this project, the goal is for you to create a Python script in a single file that will traverse the filesystem finding the largest files in a path.
+
+The function walkOS(path) is implemented recursively to get all the files including the files located inside subdirectories. The starting point is the path chosen to a directory. The function uses the os.walk(path) method
+to get the root path and the names of the directories and file. The os.path.getsize(absolute_path) method is used to get the size of the file. The os.path.splitext(absolute_path) method is used to get the name and the type of file separately.
+
+The name of the file, type and size are appended to a list. 
+The program puts the list's data into a sql table.
+Using many different sql queries some details of the files found are displayed: Number of files,larget file, smallest file and a list of the types of files found.
---
+++ os_exercise.py
@@ -0,0 +1,100 @@
+
+
+
+import os
+from turtle import clear
+from local_path import pathX
+import sqlite3
+
+
+
+
+def walkOS(path):
+
+     for root,dir,files in os.walk(path):
+
+        # print(dir)
+        # print("files: ",files,pathX)
+
+        if len(files) >0:
+
+            for _file in files:
+                absolute_path= os.path.join(root,_file)
+                size=os.path.getsize(absolute_path)
+
+                name, extension = os.path.splitext(absolute_path)  #get the type of file
+
+                #print(f"file path: {absolute_path}  ", f"Size: {size}")
+                list1.append([_file,extension,size/1000000])
+
+                dict1[_file] = size /1000000   ##size in megabytes
+
+        if len(dir) > 0:
+            for i in dir:
+                pathxx=os.path.join(root, i)
+                walkOS(pathxx)
+
+            # for i in dir:
+            #     for
+        break
+
+
+
+
+
+
+if __name__ == "__main__":
+    #main()
+    dict1={}
+    list1=[]
+
+    q1="CREATE TABLE files (id INTEGER primary key, name TEXT, type TEXT, size FLOAT)"
+    q2="INSERT INTO files(name,type,size) VALUES (?,?,?)"
+    q3="SELECT id,name, MAX(size) FROM files" 
+    q4="SELECT id,name, MIN(size) FROM files"
+    q5="SELECT * from files WHERE type = ?"
+    q6="SELECT * FROM files ORDER BY size DESC"
+    q7="SELECT DISTINCT type FROM files"
+    q8="SELECT COUNT(*) FROM files"
+    q9="SELECT type, COUNT(type) FROM files GROUP BY type ORDER BY size DESC"
+
+    walkOS(pathX)
+
+    # for k,v in dict1.items():
+    #     print(k,v)
+
+    # print(list1)
+
+    if len(list1)>0:
+        conn=sqlite3.connect(':memory:')
+        cursor=conn.cursor()
+
+        cursor.execute(q1)
+        conn.commit()
+
+        for i in list1:
+            cursor.execute(q2,i)
+            conn.commit()
+
+        cursor.execute(q3)
+        maxx=cursor.fetchone()
+        
+        cursor.execute(q4)
+        minn=cursor.fetchone()
+
+        cursor.execute(q9)
+        types=cursor.fetchall()
+
+        count=cursor.execute(q8).fetchone()
+
+        # print("max:",maxx,"minn:",minn)
+        # print("types:",types)
+
+        print(f"""
+RESULTS:
+Number of files: {count[0]}
+Largest File: {str(maxx[1])+"  "+ str(maxx[2])}MB
+Smallest File: {str(minn[1])+"  "+str(minn[2])}MB
+Type of files found: {types}
+
+        """)
